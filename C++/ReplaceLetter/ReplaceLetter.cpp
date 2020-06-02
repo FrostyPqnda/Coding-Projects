ebe742a99cdb7c5af69e94bb529bba522d663a76
FrostyPqnda <brianpham63@gmail.com>
2020-06-01 23:36:40 -0500

    Add files via upload

---
+++ C++/ReplaceLetter/ReplaceLetter.cpp
@@ -0,0 +1,48 @@
+//
+//  ReplaceLetter.cpp
+//  ReplaceLetter
+//
+//  Created by Brian Pham on 6/1/20.
+//  Copyright © 2020 FrostyPqnda. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+static string ReplaceLetter(string word, char removeLetter, char replacingLetter)
+{
+    string val = "";
+    
+    for(int i = 0; i < word.length(); i++)
+    {
+        if(word.at(i) == removeLetter)
+        {
+            val += replacingLetter;
+        }
+        else
+        {
+            val += word.at(i);
+        }
+    }
+    
+    return val;
+}
+
+int main()
+{
+    string input;
+    cout << "Enter a word: ";
+    cin >> input;
+    
+    char remove;
+    cout << "Enter a letter you want to remove: ";
+    cin >> remove;
+    
+    char replace;
+    cout << "Enter the new letter you want to add: ";
+    cin >> replace;
+    
+    string result = ReplaceLetter(input, remove, replace);
+    cout << result << endl;
+}
