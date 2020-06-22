4fe0e8200a0a0a3178e9810a400e63ad401b8990
FrostyPqnda <brianpham63@gmail.com>
2020-06-22 18:59:24 -0500

    Add files via upload

---
+++ C#/ConvertToDouble.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConvertToDouble
+{
+    class StringConversion
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a value: ");
+            double val = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Result: " + val);
+        }
+
+        static double convertToInt(string val)
+        {
+            try
+            {
+                double num = Convert.ToDouble(val);
+                return num;
+            }
+            catch(FormatException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+
+            return 0;
+        }
+    }
+}
