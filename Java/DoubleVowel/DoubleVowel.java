a69ae3b7696dde38169cdfb9ec36fad6855cacd9
FrostyPqnda <brianpham63@gmail.com>
2020-07-04 17:29:23 -0500

    Add files via upload

---
+++ Java/DoubleVowel/DoubleVowel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DoubleVowel extends DoubleVowelAppender
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a word/sentence: ");
+		String word = input.nextLine();
+		String output = doubleVowel(word);
+		System.out.println("Result: " + output);
+		input.close();
+
+	}
+
+}
---
+++ Java/DoubleVowel/DoubleVowelAppender.java
@@ -0,0 +1,43 @@
+/*
+ * This is a Java program that will add an extra vowel for any vowel in the user's
+ * input.
+ */
+public class DoubleVowelAppender 
+{
+	// This is a Public static String method that takes the String text parameter
+	// and adds a vowel if there is a vowel in the user's input.
+	public static String doubleVowel(String text) 
+	{
+		String vowel = "";
+		for (int i = 0; i < text.length(); i++) 
+		{
+			char curr = text.charAt(i);
+			if (curr == 'a') 
+			{
+				vowel += "aa"; 
+			}
+			else if(curr == 'e')
+            {
+				
+                vowel += "ee";
+            }
+			else if(curr == 'i')
+            {
+                vowel += "ii";
+            }
+            else if(curr == 'o')
+            {
+                vowel += "oo";
+            }
+            else if(curr == 'u')
+            {
+                vowel += "uu";
+            }
+            else
+            {
+                vowel += curr;
+            }
+        }
+        return vowel;
+	} 
+}
