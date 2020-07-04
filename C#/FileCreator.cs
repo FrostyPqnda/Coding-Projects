5f46d4235ad6fd6b99646931a456afc3d82fe504
FrostyPqnda <brianpham63@gmail.com>
2020-07-04 00:59:47 -0500

    Add files via upload

---
+++ C#/FileCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+/**
+ * FileCreator is a simple program that allows
+ * users to create their own files.
+ */
+namespace FileCreator
+{
+    class FileCreator
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Name your file: ");
+            string file = Console.ReadLine() + ".txt";
+            Console.WriteLine(file);
+
+            Console.Write("Write what you want in " + file + ": ");
+            string fileContent = Console.ReadLine();
+
+            File.WriteAllText(file, fileContent);
+
+            Console.WriteLine(file + " has been created.");
+        }
+    }
+}
