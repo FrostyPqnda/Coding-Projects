ef18ff6af9825780870b912e0407f4b74b1a0602
FrostyPqnda <brianpham63@gmail.com>
2020-07-04 14:21:16 -0500

    Add files via upload

--- Python/FileGenerator/FileGenerator.py
+++ Python/FileGenerator/FileGenerator.py
@@ -10,19 +10,15 @@ writes to a txt file that an user can create
 """
 
 # Names the file based on the user's input
-file_name = raw_input("Name your file: ")+".txt"
-
+file_name = input("Name your file: ")+".txt"
+          
 
 # This is where the user can type out what he/she wants
 # on their file
-UserFile = open(file_name, "w")
-write_to_file = raw_input("Type: ")
+UserFile = open("Files/"+file_name, "w")
+write_to_file = input("Enter text: ")
 UserFile.write(write_to_file)
 UserFile.close()
-print("SUCCESS! File written to " + file_name)
+print("SUCCESS! File written to " + file_name)   
 
-
-    
-    
-    
     
