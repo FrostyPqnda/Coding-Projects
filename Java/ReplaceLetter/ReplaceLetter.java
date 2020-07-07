5d8c3b2ca057fd2c24eb1e4db677c22e502d2f70
FrostyPqnda <brianpham63@gmail.com>
2020-07-07 14:47:35 -0500

    Update ReplaceLetter.java

--- Java/ReplaceLetter/ReplaceLetter.java
+++ Java/ReplaceLetter/ReplaceLetter.java
@@ -8,19 +8,19 @@
 public class ReplaceLetter 
 {
 	public static String replaceLetter(String word, char letterToReplace, char replacingLetter)
-    {
-        String letterReplacement = "";
-        for(int i = 0; i < word.length(); i++)
-        {
-            if(word.charAt(i) == letterToReplace)
-            {
-                letterReplacement += replacingLetter;
-            }
-            else
-            {
-                letterReplacement += word.charAt(i);
-            }
-        }
-        return letterReplacement;
-    }
+	{
+		String letterReplacement = "";
+		for(int i = 0; i < word.length(); i++)
+		{
+		    if(word.charAt(i) == letterToReplace)
+		    {
+			letterReplacement += replacingLetter;
+		    }
+		    else
+		    {
+			letterReplacement += word.charAt(i);
+		    }
+		}
+		return letterReplacement;
+	}
 }
