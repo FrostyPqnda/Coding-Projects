8001afeb02bd86198473c487657f70c9e9218958
FrostyPqnda <brianpham63@gmail.com>
2020-07-07 23:39:26 -0500

    Update AudioPlayer.java

--- Java/AudioPlayer/AudioPlayer.java
+++ Java/AudioPlayer/AudioPlayer.java
@@ -3,14 +3,10 @@ public class AudioPlayer
 
 	public static void main(String[] args) 
 	{
-		String gold = "Audio/Meme.wav";
-		String pizza = "Audio/Totino.wav";
 		String bee = "Audio/Bee.wav";
-		String jojo = "Audio/jojo.wav";
-		String fort = "Audio/fortnite.wav";
 		
 		AudioSource audio = new AudioSource();
-		audio.playAudio(gold);
+		audio.playAudio(bee);
 
 	}
 
