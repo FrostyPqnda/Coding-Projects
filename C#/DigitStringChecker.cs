111cda159f9d5264b0813f59eb7e0e282b01f50d
FrostyPqnda <frostypqnda@gmail.com>
2020-08-11 11:36:11 -0500

    Create DigitStringChecker.cs

---
+++ C#/DigitStringChecker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DigitStringChecker
+{
+    class DigitInString
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a text: ");
+            bool isDigit = DigitChecker(Console.ReadLine());
+
+            if(isDigit)
+            {
+                Console.WriteLine("Contains a digit!");
+            }
+            else
+            {
+                Console.WriteLine("Does not contain a digit");
+            }
+
+        }
+
+        static bool DigitChecker(String str)
+        {
+            char[] digit = str.ToCharArray();
+            bool isDigit = false;
+
+            if(str != null)
+            {
+                foreach (char c in digit)
+                {
+                    if(isDigit = Char.IsDigit(c))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return isDigit;
+        }
+    }
+}
