817d5966054dbe75ea27cbe17e24bb9d763dd626
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2020-08-23 22:37:11 -0500

    Create DigitChecker.py

---
+++ Python/DigitChecker.py
@@ -0,0 +1,25 @@
+'''
+Created on Aug 23, 2020
+
+@author: brian
+'''
+
+"""
+Method digit_checker loops through 
+the user's input to check if a number
+existed in it
+"""
+def digit_checker():
+    is_digit = False
+    text = input("Enter a text: ")
+    for char in text:
+        if char.isdigit():
+            is_digit = True
+            print("Your input contains a number")
+            return is_digit
+
+    print("Your input does not contain a number")
+    return is_digit
+
+print(digit_checker())
+
