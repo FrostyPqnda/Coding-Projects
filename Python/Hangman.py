fa7dff52f1ecced27cda8fc5237b10a7dd5de899
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2020-08-28 20:04:15 -0500

    Update Hangman.py

--- Python/Hangman.py
+++ Python/Hangman.py
@@ -10,63 +10,66 @@ Hangman game project from my CodeHS assignment
 import random
 import requests
 
-word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
-response = requests.get(word_site)
-list_of_words = response.content.splitlines()
-secret_word = random.choice(list_of_words)
-dashes = "-" * len(secret_word)
-guesses_left = len(secret_word)
-wrong = []
 
-def get_guess(word):
-    global wrong
-    global guesses_left
-    
-    while True:
-        guess = input("Guess: ")
-        guess = guess.lower()
-        guess = guess.encode()
-        
-        if len(guess) != 1:
-            print("Your guess must have exactly one character!")
-        elif not guess.isalpha():
-            print("Your guess must be a letter!")
-        elif guess not in word:
-            print("That letter is not in the secret word!")
-            guesses_left -= 1
+
+
+class Hangman:
+
+    def __init__(self):
+        self.word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+        response = requests.get(self.word_site)
+        self.list_of_words = response.content.splitlines()
+        self.secret_word = random.choice(self.list_of_words)
+        self.dashes = "-" * len(self.secret_word)
+        self.guesses_left = len(self.secret_word)
+        self.wrong = []
+
+    def get_guess(self, word):
+        while True:
+            guess = input("Guess: ")
+            guess = guess.lower()
+            guess = guess.encode()
             
-            break
-        else:
-            print("That letter is in the secret word!")
-            return guess.decode()
-            break
-    
-def update_dashes(word, dashes, guess):    
-    for i in range(len(word)):
-        if word[i] == guess:
-            new = dashes[:i]+guess+dashes[i+1:]
-            dashes = new
-    return dashes
-    
-def play_hangman():
-    # Print the word
-    print("The word is %d letters long" % len(secret_word))
-    dashes = ""
-    dashes = dashes.ljust(len(secret_word),'-')
-    
-    end = False
-    while not end:
-        print(str(guesses_left) + " guesses left.")
-        print(dashes + '        ')
-        guess = get_guess(secret_word)
-        dashes = update_dashes(secret_word.decode(), dashes, guess)
-        if dashes == secret_word.decode():
-            print("CONGRATS! YOU HAVE GUESSED THE WORD!")
-            print("The word was " + secret_word.decode())
-            end = True
-        if guesses_left == 0:
-            print("You lose. The secret word was " + secret_word.decode())
-            end = True
+            if len(guess) != 1:
+                print("Your guess must have exactly one character!")
+            elif not guess.isalpha():
+                print("Your guess must be a letter!")
+            elif guess not in word:
+                print("That letter is not in the secret word!")
+                self.guesses_left -= 1
+                break
+            else:
+                print("That letter is in the secret word!")
+                return guess.decode()
+                break
+        
+    def update_dashes(self, word, dashes, guess):    
+        for i in range(len(word)):
+            if word[i] == guess:
+                new = dashes[:i]+guess+dashes[i+1:]
+                dashes = new
+        return dashes
+        
+    def play_hangman(self):
+        # Print the word
+        print("The word is %d letters long" % len(self.secret_word))
+        dashes = ""
+        dashes = dashes.ljust(len(self.secret_word),'-')
+        
+        end = False
+        while not end:
+            print(str(self.guesses_left) + " guesses left.")
+            print(dashes + '        ')
+            guess = self.get_guess(self.secret_word)
+            dashes = self.update_dashes(self.secret_word.decode(), dashes, guess)
+            if dashes == self.secret_word.decode():
+                print("CONGRATS! YOU HAVE GUESSED THE WORD!")
+                print("The word was " + self.secret_word.decode())
+                end = True
+            if self.guesses_left == 0:
+                print("You lose. The secret word was " + self.secret_word.decode())
+                end = True
         
     
-play_hangman()
+hangman = Hangman()
+hangman.play_hangman()
