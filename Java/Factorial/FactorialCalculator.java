2e2b6f21d32a3bcf564d2dc33a594169e5fc7852
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2020-09-21 22:15:55 -0500

    Update FactorialCalculator.java

--- Java/Factorial/FactorialCalculator.java
+++ Java/Factorial/FactorialCalculator.java
@@ -6,21 +6,46 @@ import java.util.*;
  * 
  * FactorialCalculator is a java program that calculates the
  * factorial of any number
+ * 
+ * A factorial is the product of an integer and all integers before it.
+ * n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
  */
 public class FactorialCalculator 
 {
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.print("What number would you \nlike to compute the \nfactorial for? ");
+		System.out.print("What number would you like to compute the factorial for? ");
 		int digit = input.nextInt();
-		int product = 1;
+
+		System.out.println("Iteration: The factorial of " + digit + " is " + iterativeFactorial(digit));
+		System.out.println("Recursion: The factorial of " + digit + " is " + recursiveFactorial(digit));
 		
-		for(int i = digit; i > 0; i--)
-		{
-			product *= i;
-		}
-		System.out.print("The factorial of " + digit + " is " + product);
 		input.close();
+	}
+
+	/**
+	 * Method iterativeFactorial returns the value
+	 * of n factorial using iteration (for loop).
+	 */
+	public static int iterativeFactorial(int n)
+	{
+		int num = 1;
+		for(int s = n; s > 0; s--)
+			num *= s;
+
+		return num;
+	}
+
+	/**
+	 * Method recursiveFactorial returns the value
+	 * of n factorial using recusrion.
+	 */
+	public static int recursiveFactorial(int n)
+	{
+		if(n == 0)
+			return 1;
+		else
+			return n * recursiveFactorial(n - 1);
 	}
 }
