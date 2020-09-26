5b7e1d99a29b5ac8b33fdafc83ec6f750c9d0c38
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2020-09-25 22:38:32 -0500

    Updated FileCreator project

--- Java/FileCreator/FileCreator.java
+++ Java/FileCreator/FileCreator.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.File;
 /**
  * 
- * @author brian
+ * @author FrostyPqnda
  *
  * FileCreator is a simple txt file writer 
  * that users can write to
@@ -13,24 +13,37 @@ public class FileCreator
 {
 	public static void main(String[] args) 
 	{
+		// Creates a scanner object for user input
+		// and asks users to name their files.
+		Scanner input = new Scanner(System.in); 
+		System.out.print("Name your file: ");
+		
+		// Allows users to name their files 
+		// and will convert it to .txt format
+		String fileInput = input.nextLine();
+		String fileName = fileInput.concat(".txt");
+
 		try
-		{
-			Scanner input = new Scanner(System.in);
-			// File is being named
-			System.out.print("Name your file: ");
-			String file_name_input = input.nextLine();
-			String file_name = file_name_input.concat(".txt");
+		{	
+			File path = new File("D:\\Coding-Projects\\Java\\FileCreator\\Files"); // Sets the directory where the file will be save
+			File file = new File(path, fileName); // Creates a new File object with the parent directory and the .txt file the user created
+			FileWriter fw = new FileWriter(file); // Creates a FileWriter object to allow users to write what they want to their files
 			
-			// File content is being created
-			FileWriter user_file = new FileWriter(file_name);
+			// Asks users what they want to put into the content of their
+			// .txt files
 			System.out.println("Type out what you want on your txt file: ");
 			String text = input.nextLine();
-			user_file.write(text);
 			
-			// File is finished
-			user_file.close();
+			// Writes what the user inputted into the file
+			// and closes it after finishing
+			fw.write(text);
+			fw.close();
+			
+			// Closes the scanner object and prints if the file
+			// was successfully written and where it was saved
 			input.close();
-			System.out.println("SUCCESS! File written to " + file_name);
+			System.out.println("SUCCESS! File written to " + fileName);
+			System.out.println(fileName + " was saved in " + path.getPath());
 		}
 		catch (IOException e)
 		{
