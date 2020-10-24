21df0a4142d64c2d910755a0edd1c6e5ae93cfd9
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2020-10-23 23:25:10 -0500

    Update

--- Java/FindMinMax/FindMinMax.java
+++ Java/FindMinMax/FindMinMax.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Java program that finds the smallest integer
@@ -8,7 +7,7 @@ import java.util.Collections;
 public class FindMinMax
 {
     /**
-     * Method nestedFindMinimum uses a nested for-loop to sort @param numbers
+     * Method findMinimum uses a nested for-loop to sort @param numbers
      * and find the smallest integer in the ArrayList
      */
     public static int findMinimum(ArrayList<Integer> numbers)
@@ -16,12 +15,14 @@ public class FindMinMax
         if(numbers.size() == 1 || numbers == null)
             return numbers.get(0);
         // Sorts the ArrayList in descending order using a nested for-loop
-        for(int x = 0; x < numbers.size(); x++)
-            for(int y = numbers.size() - 1; y > x; y--)
+        for(int x = 0; x < numbers.size(); x++) {
+            for(int y = numbers.size() - 1; y > x; y--) {
                 if(numbers.get(x) > numbers.get(y)) {
                     numbers.set(x, numbers.get(y)); 
                     numbers.set(y, numbers.get(x));
                 }
+            }
+        }
         // Removes the highest number in the ArrayList until only one element is left.
         for(int i = numbers.size() - 1; i > 0; i--)
             numbers.remove(i);
@@ -29,7 +30,7 @@ public class FindMinMax
     }
 
     /**
-     * Method nestedLoopFindMaximum uses a nested for-loop to sort @param numbers
+     * Method findMaximum uses a nested for-loop to sort @param numbers
      * and find the largest integer in the ArrayList
      */
     public static int findMaximum(ArrayList<Integer> numbers)
@@ -37,12 +38,14 @@ public class FindMinMax
         if(numbers.size() == 1 || numbers == null)
             return numbers.get(0);
         // Sorts the ArrayList in ascending order using a nested for-loop
-        for(int x = 0; x < numbers.size(); x++)
-            for(int y = numbers.size() - 1; y > x; y--)
+        for(int x = 0; x < numbers.size(); x++) {
+            for(int y = numbers.size() - 1; y > x; y--) {
                 if(numbers.get(x) < numbers.get(y)) {
                     numbers.set(x, numbers.get(y));
                     numbers.set(y, numbers.get(x));
                 }
+            }
+        }
         // Removes the smallest number in the ArrayList until only one element is left.
         for(int i = numbers.size() - 1; i > 0; i--)
             numbers.remove(i);
