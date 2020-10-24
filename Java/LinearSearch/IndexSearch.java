2bc1aac78aa124a9fddd835009bc7ab42144b7b6
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2020-10-24 18:41:06 -0500

    Update LinearSearch project

--- Java/LinearSearch/IndexSearch.java
+++ Java/LinearSearch/IndexSearch.java
@@ -1,34 +1,19 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class IndexSearch extends LinearSearchAlgorithm
 {
     public static void main(String[] args)
     {
-        Random rand = new Random(12345);
-        /* Currently not working. Not generating random value when using these two variables. */
-        //int randHP = rand.nextInt(714) + 1; 
-        //int randLVL = rand.nextInt(100) + 1;
+        String[][] str = {
+            {"Bird", "Dog", "Cat"},
+            {"Dragon", "Wolf", "Tiger"},
+            {"Gecko", "Zebra", "Penguin"}
+        };
+        
+        // Index is found
+        System.out.println(array2DSearch(str, "Gecko"));
 
-        Pokemon salamence = new Pokemon("Salamence", "Dragon", "Flying", "Intimidate", "Moxie", true, true, false, ((int)(Math.random() * 714) + 1), rand.nextInt(100) + 1);
-        Pokemon charizard = new Pokemon("Charizard", "Fire", "Flying", "Solar Power", "Blaze", false, true, false, ((int)(Math.random() * 714) + 1), rand.nextInt(100) + 1);
-        Pokemon lucario = new Pokemon("Lucario", "Fighting", "Steel", "Inner Focus", "Justified", true, true, true, ((int)(Math.random() * 714) + 1), rand.nextInt(100) + 1);
-        Pokemon blastoise = new Pokemon("Blastoise", "Water", "None", "Torrent", "Rain Dish", true, true, true, ((int)(Math.random() * 714) + 1), rand.nextInt(100) + 1);
-        Pokemon gengar = new Pokemon("Gengar", "Ghost", "Poison", "Cursed Body", "Levitate", true, true, true, ((int)(Math.random() * 714) + 1), rand.nextInt(100) + 1);
-        Pokemon infernape = new Pokemon("Infernape", "Fire", "Fighting", "Blaze", "Iron Fist", true, false, false, ((int)(Math.random() * 714) + 1), rand.nextInt(100) + 1);
-        
-        ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
-        pokemon.add(salamence);
-        pokemon.add(charizard);
-        pokemon.add(lucario);
-        pokemon.add(blastoise);
-        pokemon.add(gengar);
-        pokemon.add(infernape);
-        
-        int index = arrayListSearch(pokemon, salamence);
-
-        System.out.println("\n"+ salamence);
-        System.out.println("\nFound at index: " + index);
-        
+        // Index is not found
+        System.out.println(array2DSearch(str, "Lion"));
     }
 }
--- Java/LinearSearch/LinearSearchAlgorithm.java
+++ Java/LinearSearch/LinearSearchAlgorithm.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class LinearSearchAlgorithm extends Pokemon
+public class LinearSearchAlgorithm
 {
     /**
      * Method arraySearch loops through 
@@ -17,6 +17,7 @@ public class LinearSearchAlgorithm extends Pokemon
                 return i;
         return -1;
     }
+
     /**
      * Method arrayListSearch loops through 
      * the Integer ArrayList of @param arr 
@@ -32,6 +33,24 @@ public class LinearSearchAlgorithm extends Pokemon
                 return i;
         return -1;
     }
+
+    /**
+     * Method array2DSearch loops through
+     * the 2D array of integers @param arr to find
+     * the index of @param key
+     * 
+     * If @param key exist in @param arr
+     * the method will return at what index
+     */
+    public static String array2DSearch(int[][] arr, int key)
+    {
+        for(int row = 0; row < arr.length; row++)
+            for(int col = 0; col < arr[row].length; col++)
+                if (arr[row][col] == key)
+                    return "The index of " + key + " is at row " + row + ", col " + col;
+        return "The index of " + key + " was not found";
+    }
+
     /**
      * Method arraySearch loops through 
      * the double array of @param arr for 
@@ -47,6 +66,7 @@ public class LinearSearchAlgorithm extends Pokemon
                 return i;
         return -1;
     }
+
     /**
      * Method arrayListSearch loops through 
      * the Double ArrayList of @param arr 
@@ -62,6 +82,24 @@ public class LinearSearchAlgorithm extends Pokemon
                 return i;
         return -1;
     }
+
+    /**
+     * Method array2DSearch loops through
+     * the 2D array of doubles @param arr to find
+     * the index of @param key
+     * 
+     * If @param key exist in @param arr
+     * the method will return at what index
+     */
+    public static String array2DSearch(double[][] arr, double key)
+    {
+        for(int row = 0; row < arr.length; row++)
+            for(int col = 0; col < arr[row].length; col++)
+                if (arr[row][col] == key)
+                    return "The index of " + key + " is at row " + row + ", col " + col;
+        return "The index of " + key + " was not found";
+    }
+    
     /**
      * Method arraySearch loops through 
      * the String array of @param arr for 
@@ -73,10 +111,11 @@ public class LinearSearchAlgorithm extends Pokemon
     public static int arraySearch(String[] arr, String key)
     {
         for(int i = 0; i < arr.length; i++)
-            if(arr[i].equalsIgnoreCase(key))
+            if(arr[i].equals(key))
                 return i;
         return -1;
     }
+
     /**
      * Method arrayListSearch loops through 
      * the String ArrayList of @param arr 
@@ -88,15 +127,33 @@ public class LinearSearchAlgorithm extends Pokemon
     public static int arrayListSearch(ArrayList<String> arr, String key)
     {
         for(int i = 0; i < arr.size(); i++)
-            if(arr.get(i).equalsIgnoreCase(key))
+            if(arr.get(i).equals(key))
                 return i;
         return -1;
     }
+
+    /**
+     * Method array2DSearch loops through
+     * the 2D array of Strings @param arr to find
+     * the index of @param key
+     * 
+     * If @param key exist in @param arr
+     * the method will return at what index
+     */
+    public static String array2DSearch(String[][] arr, String key)
+    {
+        for(int row = 0; row < arr.length; row++)
+            for(int col = 0; col < arr[row].length; col++)
+                if(arr[row][col].equals(key))
+                    return "The index of " + key + " is at row " + row + ", col " + col;
+        return "The index of " + key + " was not found";
+    }
+
     /**
      * Method arraySearch loops through 
      * the Character array of @param arr 
      * for @param key.
-     * 
+     *
      * If @param key exist in @param arr
      * the method will return at what index
      */
@@ -107,11 +164,12 @@ public class LinearSearchAlgorithm extends Pokemon
                 return i;
         return -1;
     }
+
     /**
      * Method arrayListSearch loops through 
      * the Character ArrayList of @param arr 
      * for @param key.
-     * 
+     *
      * If @param key exist in @param arr
      * the method will return at what index
      */
@@ -122,35 +180,22 @@ public class LinearSearchAlgorithm extends Pokemon
                 return i;
         return -1;
     }
+    
     /**
-     * Method arraySearch loops through 
-     * the Pokemon array of @param arr 
-     * for @param key.
-     * 
+     * Method array2DSearch loops through
+     * the 2D array of Character @param arr to find
+     * the index of @param key
+     *
      * If @param key exist in @param arr
      * the method will return at what index
      */
-    public static int arraySearch(Pokemon[] arr, Pokemon key)
+    public static String array2DSearch(char[][] arr, char key)
     {
-        for(int i = 0; i < arr.length; i++)
-            if(arr[i].equals(key))
-                return i;
-        return -1;
-    }
-    /**
-     * Method arrayListSearch loops through 
-     * the Pokemon ArrayList of @param arr 
-     * for @param key.
-     * 
-     * If @param key exist in @param arr
-     * the method will return at what index
-     */
-    public static int arrayListSearch(ArrayList<Pokemon> arr, Pokemon key)
-    {
-        for(int i = 0; i < arr.size(); i++)
-            if(arr.get(i).equals(key))
-                return i;
-        return -1;
+        for(int row = 0; row < arr.length; row++)
+            for(int col = 0; col < arr[row].length; col++)
+                if(arr[row][col] == key)
+                    return "The index of " + key + " is at row " + row + ", col " + col;
+        return "The index of " + key + " was not found";
     }
 
 }
