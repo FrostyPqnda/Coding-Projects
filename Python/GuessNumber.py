62adbe58e50c100e9328724fe5adce30fe2738f8
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-01-06 19:05:28 -0600

    Create GuessNumber.py

---
+++ Python/GuessNumber.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 6, 2021
+
+@author: brian
+'''
+from random import randint
+"""
+Python game that asks users to guess a number
+between 1 and 100 inclusive.
+"""
+randNum = randint(1, 100) # Generated random number b/w 1 and 100 inclusive
+count = 1 # Counting variable that counts how many tries it took
+
+while True:
+    num = int(input("Guess the number [1, 100]: "))
+
+    if num == randNum:
+        print("Correct! \nIt took you " + str(count) + " tries to guess the number.")
+        break
+    elif num != randNum:
+        print("Try again!")
+    
+    count += 1
