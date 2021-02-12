bb8d5bd487b50f2e3a96def1beaa7df2d0c521b1
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-02-11 23:56:45 -0600

    d

--- Python/Hangman.py
+++ Python/Hangman.py
@@ -12,30 +12,30 @@ import requests
 class Hangman:
 
     def __init__(self):
-        self.word_site = "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"
+        self.word_site = 'https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt'
         response = requests.get(self.word_site)
         self.list_of_words = response.content.splitlines()
         self.secret_word = random.choice(self.list_of_words)
-        self.dashes = "-" * len(self.secret_word)
+        self.dashes = '-' * len(self.secret_word)
         self.guesses_left = len(self.secret_word)
         self.wrong = []
 
     def get_guess(self, word):
         while True:
-            guess = input("Guess: ")
+            guess = input('Guess: ')
             guess = guess.lower()
             guess = guess.encode()
             
             if len(guess) != 1:
-                print("Your guess must have exactly one character!")
+                print('Your guess must have exactly one character!')
             elif not guess.isalpha():
-                print("Your guess must be a letter!")
+                print('Your guess must be a letter!')
             elif guess not in word:
-                print("That letter is not in the secret word!")
+                print('That letter is not in the secret word!')
                 self.guesses_left -= 1
                 break
             else:
-                print("That letter is in the secret word!")
+                print('That letter is in the secret word!')
                 return guess.decode()
                 break
         
@@ -48,22 +48,22 @@ class Hangman:
         
     def play_hangman(self):
         # Print the word
-        print("The word is %d letters long" % len(self.secret_word))
-        dashes = ""
+        print('The word is %d letters long' % len(self.secret_word))
+        dashes = ''
         dashes = dashes.ljust(len(self.secret_word),'-')
         
         end = False
         while not end:
-            print(str(self.guesses_left) + " guesses left.")
+            print(str(self.guesses_left) + ' guesses left.')
             print(dashes + '        ')
             guess = self.get_guess(self.secret_word)
             dashes = self.update_dashes(self.secret_word.decode(), dashes, guess)
             if dashes == self.secret_word.decode():
-                print("CONGRATS! YOU HAVE GUESSED THE WORD!")
-                print("The word was " + self.secret_word.decode())
+                print('CONGRATS! YOU HAVE GUESSED THE WORD!')
+                print('The word was ' + self.secret_word.decode())
                 end = True
             if self.guesses_left == 0:
-                print("You lose. The secret word was " + self.secret_word.decode())
+                print('You lose. The secret word was ' + self.secret_word.decode())
                 end = True
         
     
