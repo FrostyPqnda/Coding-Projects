0e43a697be14e3d37e47cbd040a0d649269753f0
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-02-12 00:01:06 -0600

    Update VowelCount.java

--- Java/VowelCount/VowelCount.java
+++ Java/VowelCount/VowelCount.java
@@ -11,13 +11,9 @@ public class VowelCount
 	public static int countVowel(String sentence, char vowel)
 	{
 		int countForVowels = 0;
-		String check = "";
 		for(int i = 0; i < sentence.length(); i++)
-		{
-		    check += sentence.charAt(i);
-		    if (check.charAt(i) == vowel)
+		    if(sentence.charAt(i) == vowel)
 				countForVowels++;
-		}
 		return countForVowels;
 	}
 }
