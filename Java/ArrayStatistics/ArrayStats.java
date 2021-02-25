2ec6e36c9358bd7696d7f3e6159b5d1a18a4eb96
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-02-25 15:21:48 -0600

    Update ArrayStats.java

--- Java/ArrayStatistics/ArrayStats.java
+++ Java/ArrayStatistics/ArrayStats.java
@@ -2,7 +2,26 @@ public class ArrayStats extends Statistics
 {
     public static void main(String[] args)
     {
-        int[] arr = {10, 3, 3, 11, 11, 11, 100};
-        System.out.println(findStandardDeviation(arr));
+        int[] arr = randomArray(10);
+        System.out.print("Data Set = { ");
+        for(int i : arr)
+            System.out.print(i + " ");
+        System.out.println("}");
+        System.out.println("Median: " + findMedian(arr));
+        System.out.println("Mode: " + findMode(arr));
+        System.out.println("Mean: " + findMean(arr));
+        System.out.println("Range: " + findRange(arr));
+        System.out.println("Standard Deviation: " + findStandardDeviation(arr));
+    }
+
+    static int[] randomArray(int size)
+    {
+        int[] arr = new int[size];
+        for(int i = 0; i < arr.length; i++)
+        {
+            int value = (int)(Math.random() * 91) + 10;
+            arr[i] = value;
+        }
+        return arr;
     }
 }
