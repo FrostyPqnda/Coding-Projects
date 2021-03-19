55624d6f68979ee737a789240f6918d9218dddce
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-03-19 13:32:40 -0500

    Update WordSearch.py

--- Python/WordSearch.py
+++ Python/WordSearch.py
@@ -5,13 +5,11 @@ returns the index position it is first
 located in the sentence.
 """
 def search(phrase, word):
-    if phrase[-1] == '.' or phrase[-1] == '?' or phrase[-1] == '!':
-        phrase = phrase[:-1]
     textArr = phrase.split(' ')
     for i in range(0, len(textArr)):
         if(textArr[i] == word):
             return 'The word [' + word + '] exists in the sentence and is located at index ' + str(i) 
     return 'The word [' + word + '] does not exist in the sentence'
 
-s = 'I am a cat.'
+s = 'I am a cat'
 print(search(s, 'cat'))
