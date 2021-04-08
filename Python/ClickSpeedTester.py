5ba556d3cf86f8461c4d800bfadcf7424ace4010
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-04-07 22:16:12 -0500

    Update ClickSpeedTester.py

--- Python/ClickSpeedTester.py
+++ Python/ClickSpeedTester.py
@@ -8,12 +8,12 @@ cps = 0
 def clicks():
     global count
     count += 1
-    counterLabel.configure(text = f'Clicks: {count}')
+    clickCounterLabel.configure(text = f'Clicks: {count}')
 
 # Countdown timer that disables the button once it hits 0
 def countdown(t):
     resetBtn.configure(state = 'disabled')
-    timerLabel.configure(text = 'Timer: ' + str(t))
+    countdownTimerLabel.configure(text = 'Timer: ' + str(t))
     if t > 0:
         root.after(1000, countdown, t - 1)
     if t == 0:
@@ -32,7 +32,7 @@ def reset():
     global cps
     count = 0
     cps = 0
-    counterLabel.configure(text = 'Clicks: ' + str(count))
+    clickCounterLabel.configure(text = 'Clicks: ' + str(count))
     cpsLabel.configure(text = 'CPS: ' + str(cps))
     clickBtn.configure(state = 'active')
     countdown(10)
@@ -43,26 +43,24 @@ will be displayed.
 """
 root = Tk()
 root.configure(bg = '#283140')
-width= root.winfo_screenwidth() 
-height= root.winfo_screenheight()
-root.geometry("%dx%d" % (width, height))
+root.geometry("%dx%d" % (root.winfo_screenwidth(), root.winfo_screenheight()))
 root.title('Click Speed Tester')
 
-# Creates a title text and adds it to the app. 
+# Giant title text
 title = Label(root, font = ('Oswald', 40, 'bold'), background = '#283140', foreground = '#cccccc', text='CLICK SPEED TEST')
 title.pack()
 
-# Creates a canvas board for the app where most of the elements such as the click button will go.
+# A "board" where all of the buttons and labels will be displayed on
 canvas = Canvas(root, height = 700, width = 700, bg = '#1d4d80')
 canvas.pack(expand=True, fill='both')
 
-# Creates a label that displays the user's click count
-counterLabel = Label(canvas, text = 'Clicks: 0', bg = '#222222', fg = '#cccccc', font = ('Oswald', 20, 'bold'))
-counterLabel.pack(pady = 10)
+# Creates a label that displays the number of clicks the user has made
+clickCounterLabel = Label(canvas, text = 'Clicks: 0', bg = '#222222', fg = '#cccccc', font = ('Oswald', 20, 'bold'))
+clickCounterLabel.pack(pady = 10)
 
 # Creates a label that displays the countdown timer
-timerLabel = Label(canvas, text = 'Timer: ', bg = '#222222', fg = '#cccccc', font = ('Oswald', 20, 'bold'))
-timerLabel.pack(pady = 20)
+countdownTimerLabel = Label(canvas, text = 'Timer: ', bg = '#222222', fg = '#cccccc', font = ('Oswald', 20, 'bold'))
+countdownTimerLabel.pack(pady = 20)
 
 # Creates a label that displays the user's average clicks per second
 cpsLabel = Label(canvas, text = 'CPS: ' + str(cps), bg = '#222222', fg = '#cccccc', font = ('Oswald', 20, 'bold'))
