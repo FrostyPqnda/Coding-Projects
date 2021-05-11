9ae3db37991d450f695a873ff61c4737ed35c21d
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-05-11 12:55:20 -0500

    Create Sequence.py

---
+++ Python/Sequence.py
@@ -0,0 +1,44 @@
+from math import pow
+
+
+# Checks if the array is an arithmetic sequence
+def isArithmetic(arr):
+    isSequential = True
+    for i in range(0, len(arr) - 2):
+        if arr[i + 1] - arr[i] != arr[i + 2] - arr[i + 1]:
+            isSequential = False
+    return isSequential
+
+"""
+Calculates the nth term of an arithmetic sequence
+
+a₁: First term of the seqeunce
+d: Common difference in the sequence
+n: Nth term of the sequence
+
+Formula: aₙ = a₁ + d(n - 1)
+"""
+def arithmeticNthTerm(a1, d, n):
+    nthTerm = a1 + d * (n - 1)
+    return nthTerm
+
+# Checks if the array is a geometric sequence
+def isGeometric(arr):
+    isSequential = True
+    for i in range(0, len(arr) - 2):
+        if arr[i + 1] / arr[i] != arr[i + 2] / arr[i + 1]:
+            isSequential = False
+    return isSequential
+
+"""
+Calculates the nth term of a geometric sequence
+
+a₁: First term of the seqeunce
+r: Common ratio in the sequence
+n: Nth term of the sequence
+
+Formula: aₙ = a₁ * r⁽ⁿ⁻¹⁾
+"""
+def geometricNthTerm(a1, r, n):
+    nthTerm = a1 * pow(r, (n - 1))
+    return nthTerm
