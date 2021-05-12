94e0ac48ef10c0648ae472a12f65510bdd0457ca
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-05-12 12:24:34 -0500

    Update MatrixSorter.cs

--- C#/DimensionalArraySorter/MatrixSorter.cs
+++ C#/DimensionalArraySorter/MatrixSorter.cs
@@ -37,32 +37,35 @@ namespace DimensionalArraySorter
         }
 
         // Sorts the given matrix [Edited version of GeeksForGeeks' Matrix Sort - https://www.geeksforgeeks.org/sort-given-matrix/]
-        static void SortMatrix(int[,] curr, int rowLength, int columnLength)
+        static void SortMatrix(int[,] mat, int rowSize, int colSize)
         {
-            int[] arr = new int[rowLength * columnLength]; // Create a temporary array with a length of @param rowLength * columnLength
+            int[] arr = new int[rowSize * colSize]; // Create a temporary array with a length of @param rowSize * colSize
             int count = 0; // Counter variable for the temporary array
 
             // Copy elements of the multi-dimensional arrays one by one into the temporary array
-            for(int row = 0; row < rowLength; row++)
-                for(int col = 0; col < columnLength; col++)
-                    arr[count++] = curr[row, col];
+            for(int row = 0; row < rowSize; row++)
+                for(int col = 0; col < colSize; col++)
+                    arr[count++] = mat[row, col];
 
             QuickSorter qs = new QuickSorter(); // Create object of the QuickSorter class
             qs.QuickSort(arr, 0, arr.Length - 1); // Calls the QuickSort function to sort the temporary array
 
             count = 0; // Resets the counter variable back to zero
             // Copy elements of the temporary array one by one into the multi-dimensional array
-            for(int row = 0; row < rowLength; row++)
-                for(int col = 0; col < columnLength; col++)
-                    curr[row, col] = arr[count++];
+            for(int row = 0; row < rowSize; row++)
+                for(int col = 0; col < colSize; col++)
+                    mat[row, col] = arr[count++];
         }
 
         // Print all elements of a multi-dimensional array
-        static void PrintMatrix(int[,] curr)
+        static void PrintMatrix(int[,] mat)
         {
-            for(int row = 0; row < curr.GetLength(0); row++) {
-                for(int col = 0; col < curr.GetLength(1); col++)
-                    Console.Write(curr[row, col] + "\t");
+            for(int row = 0; row < mat.GetLength(0); row++) 
+            {
+                for(int col = 0; col < mat.GetLength(1); col++)
+                {
+                    Console.Write(mat[row, col] + "\t");
+                }
                 Console.WriteLine();
             }
         }
