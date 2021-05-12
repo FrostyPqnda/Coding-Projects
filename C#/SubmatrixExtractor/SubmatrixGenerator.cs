e8cfcfe9fd1eb5863a76e2527baa3cea66b590b4
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-05-11 22:48:47 -0500

    Created SubmatrixExtractor project

---
+++ C#/SubmatrixExtractor/SubmatrixGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace SubmatrixExtractor
+{
+    class SubmatrixGenerator
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Submatrix Extractor";
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            int[,] mat = GenerateRandomMatrix(5, 4);
+            Console.WriteLine("Original: \n");
+            PrintMatrix(mat);
+
+            Console.WriteLine("\nSubmatrix: \n");
+            int[,] subMat = ExtractSubmatrix(mat, 1, 3, 1, 3);
+            PrintMatrix(subMat);
+
+            Console.ReadKey();
+        }
+        
+        // Extracts a submatrix from the original matrix
+        // Condition: 0 ≤ startRow < endRow, 0 ≤ startCol < endCol
+        static int[,] ExtractSubmatrix(int[,] mat, int startRow, int endRow, int startCol, int endCol)
+        {
+            int rowSize = endRow - startRow, colSize = endCol - startCol;
+            int[,] subMat = new int[rowSize, colSize];
+
+            for(int row = startRow; row < endRow; row++)
+            {
+                for(int col = startCol; col < endCol; col++)
+                {
+                    int r = row - startRow, c = col - startCol;
+                    subMat[r, c] = mat[row, col];
+                }
+            }
+
+            return subMat;
+        }
+
+
+        // Creates a randomized 2D array with a row length of @param rowLength and a column length of @param columnLength
+        static int[,] GenerateRandomMatrix(int rowSize, int colSize)
+        {
+            int[,] arr = new int[rowSize, colSize]; // Create a 2D array with a size of @param rowLength * @param columnLength
+            Random rng = new Random(); // Create object from the Random class
+
+            // Populate the 2D array with random value from the range of [100, 999] inclusive
+            for(int row = 0; row < rowSize; row++)
+                for(int col = 0; col < colSize; col++)
+                    arr[row, col] = rng.Next(100, 1000);
+
+            return arr; // Return the randomized 2D array
+        }
+
+        // Print all elements of a multi-dimensional array
+        static void PrintMatrix(int[,] mat)
+        {
+            for(int row = 0; row < mat.GetLength(0); row++) 
+            {
+                for(int col = 0; col < mat.GetLength(1); col++)
+                {
+                    Console.Write(mat[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
