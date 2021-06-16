50e606ba9c98bc04efa6ce99feeb10f17ef3fd78
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-06-15 23:14:16 -0500

    Create ImaginaryNumber.py

---
+++ Python/ImaginaryNumber.py
@@ -0,0 +1,42 @@
+from math import *
+
+# Function to check if n is perfect square.
+def isPerfectSquare(n):
+    sqrtNum = sqrt(n)
+    return (sqrtNum - floor(sqrtNum)) == 0
+
+"""
+Returns the root of a negative number.
+
+If n is greater than 0, the function will
+just return the root value of n.
+"""
+def getImaginaryNumber(n):
+    imgNum = ''
+    
+    """
+    If n is less than 0, return the root value
+    of n but with an 'i' attached. 
+
+    Otherwise, return the root value of n.
+    """
+    if(n < 0):
+        num = sqrt(abs(n))
+        if(isPerfectSquare(abs(n))):
+            intNum = int(num)
+            imgNum += str(intNum) + 'i'
+        else:
+            roundedNum = float(round(num * 100)) / 100 # Rounds the number to the nearest hundredth place.
+            imgNum += str(roundedNum) + 'i'
+    else:
+        num = sqrt(n)
+        if(isPerfectSquare(n)):
+            intNum = int(num)
+            imgNum += str(intNum)
+        else:
+            roundedNum = float(round(num * 100)) / 100 # Rounds the number to the nearest hundredth place.
+            imgNum += str(roundedNum)
+
+    return imgNum
+
+print(getImaginaryNumber(-101))
