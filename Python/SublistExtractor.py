7752253475f28a8f069dcedb0c8e15b4020f646d
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-06-19 18:41:25 -0500

    Nonimportant update to code

---
+++ Python/SublistExtractor.py
@@ -0,0 +1,12 @@
+# Extracts a sublist from the original list.
+# Values of sublist will include all values
+# of the original list [start, end).
+# Precondition: 0 < start < end < list length
+def extractSublist(arr, start, end):
+    subList = []
+    for item in range(start, end):
+        subList.append(arr[item])
+    return subList
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(extractSublist(arr, 5, 8))
