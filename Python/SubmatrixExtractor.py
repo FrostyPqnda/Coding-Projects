a3d0b9921ee6d9fb645d7e65f4d5e81bf9108a96
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-06-26 18:37:41 -0500

    Update SubmatrixExtractor.py

--- Python/SubmatrixExtractor.py
+++ Python/SubmatrixExtractor.py
@@ -1,19 +1,33 @@
 from random import randint
 
-# Creates a submatrix from the original matrix.
-# Size of submatrix is endRow - startRow, endCol - startCol.
-# Submatrix will contain all values of the original matrix
-# from [startRow, endRow] and [startCol, endCol]
-def extractSubmatrix(mat, startRow, endRow, startCol, endCol):
-    rowSize = endRow - startRow
-    colSize = endCol - startCol
-    subMat = [[None for _ in range(colSize)] for _ in range(rowSize)]
-    for row in range(startRow, endRow):
-        for col in range(startCol, endCol):
-            r = row - startRow
-            c = col - startCol
-            subMat[r][c] = mat[row][col] 
-    return subMat
+"""
+Creates a submatrix with values extracted from the original
+matrx.
+
+If values for the endRow and endCol are given, create a 
+submatrix with a size of endRow - row, endCol - col.
+
+Otherwise, submatrix will be created with values of the original
+matrix from [0, row] and [0, col].
+"""
+def extractSubmatrix(mat, row, col, endRow = None, endCol = None):
+    
+    if endRow is not None and endCol is not None:
+        rowSize = endRow - row
+        colSize = endCol - col
+        subMat = [[None for _ in range(colSize)] for _ in range(rowSize)]
+        for r in range(row, endRow):
+            for c in range(col, endCol):
+                x = r - row
+                y = c - col
+                subMat[x][y] = mat[r][c] 
+        return subMat
+    else:
+        subMat = [[None for _ in range(col + 1)] for _ in range(row + 1)]
+        for r in range(row + 1):
+            for c in range(col + 1):
+                subMat[r][c] = mat[r][c]
+        return subMat
 
 # Creates a 2D list with values [100, 999] inclusive.
 # 2D list will have a defined row size and column size.
@@ -32,9 +46,9 @@ def printMatrix(mat):
         print()
 
 print('Original:\n')
-mat = createMatrix(3, 3)
+mat = createMatrix(5, 5)
 printMatrix(mat)
 
 print('\nSubmatrix:\n')
-subMat = extractSubmatrix(mat, 1, 1)
+subMat = extractSubmatrix(mat, 2, 3)
 printMatrix(subMat)
