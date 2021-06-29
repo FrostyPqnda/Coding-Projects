447a11940d15f8a9b025d1ec3caa2b49f6f97dd2
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-06-28 20:48:59 -0500

    Update TicTacToe.py

--- Python/TicTacToe.py
+++ Python/TicTacToe.py
@@ -28,31 +28,26 @@ def get_valid_index(prompt):
 def game_is_over(board):
     for i in range(3):
         # Check horizontal
-        if board[i][0] == board[i][1] == board[i][2] \
-            and board[i][0] != " ":
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != " ":
             print (board[i][0] + " wins!")
             return True
         
         # Check vertical
-        if board[0][i] == board[1][i] == board[2][i] \
-            and board[0][i] != " ":
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != " ":
             print (board[0][i] + " wins!")
             return True
         
     # Check diagonals
-    if board[0][0] == board[1][1] == board[2][2] \
-        and board[0][0] != " ":
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
         print (board[0][0] + " wins!")
         return True
     
-    if board[2][0] == board[1][1] == board[0][2] \
-        and board[2][0] != " ":
+    if board[2][0] == board[1][1] == board[0][2] and board[2][0] != " ":
         print (board[2][0] + " wins!")
         return True
     
     # Check tie
-    if " " not in board[0] and " " not in board[1] \
-        and " " not in board[2]:
+    if " " not in board[0] and " " not in board[1] and " " not in board[2]:
         print ("Tie game!")
         return True
     
@@ -78,7 +73,7 @@ def play_tic_tac_toe():
     for i in range(9):
         print_board(board)
         print ("It's " + turn + "'s turn.")
-        row = get_valid_index("Row: ")
+        row = get_valid_index("\nRow: ")
         col = get_valid_index("Col: ")
         
         if board[row][col] == " ":
