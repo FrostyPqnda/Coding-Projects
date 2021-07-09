9c03f8b9847fb8b781513abba833335d70899843
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-07-08 20:38:40 -0500

    Update MatrixExpander.cs

--- C#/MatrixExpander/MatrixExpander.cs
+++ C#/MatrixExpander/MatrixExpander.cs
@@ -10,8 +10,9 @@ namespace MatrixExpander
             Console.Title = "Matrix Expander";
 
             int[,] mat = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
             };
 
             Console.WriteLine("Original Matrix:\n");
