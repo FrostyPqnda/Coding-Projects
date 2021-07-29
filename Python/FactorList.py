30942f78b4159477322c891dfcb88883e2b7d44e
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-07-28 22:19:06 -0500

    Update FactorList.py

--- Python/FactorList.py
+++ Python/FactorList.py
@@ -4,9 +4,17 @@ class FactorList:
         self.factorList = []
         self.number = number
 
-        for i in range(1, self.number + 1):
-            if self.number % i == 0:
-                self.factorList.append(i)
+        if self.number > 0:
+            for i in range(1, self.number + 1):
+                if self.number % i == 0:
+                    self.factorList.append(i)
+        else:
+            for i in range(self.number, abs(self.number) + 1):
+                if i == 0:
+                    continue
+                else:
+                    if(number % i == 0):
+                        self.factorList.append(i)
     
     def __str__(self):
         return str(self.factorList)
