99ab2d410bd53d2029b9679812c5283f40c80bf9
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-08-01 09:43:43 -0500

    Update MatrixExpander.py

--- Python/MatrixExpander.py
+++ Python/MatrixExpander.py
@@ -1,36 +1,35 @@
-class MatrixExpander:
+def expandMatrix(mat, rowFactor, colFactor):
+    rowSize = len(mat) * rowFactor
+    colSize = len(mat[0]) * colFactor
 
-    def __init__(self, mat, rowFactor, colFactor):
-        
-        rowSize = len(mat) * rowFactor
-        colSize = len(mat[0]) * colFactor
+    expMat = [[None for _ in range(colSize)] for _ in range(rowSize)]
 
-        self.expMat = [[None for _ in range(colSize)] for _ in range(rowSize)]
+    for r in range(len(mat)):
+        rowOffset = r * rowFactor
+        rowLen = rowOffset + rowFactor
 
-        for r in range(len(mat)):
-            rowOffset = r * rowFactor
-            rowLen = rowOffset + rowFactor
+        for c in range(len(mat[0])):
+            colOffset = c * colFactor
+            colLen = colOffset + colFactor
 
-            for c in range(len(mat[0])):
-                colOffset = c * colFactor
-                colLen = colOffset + colFactor
+            for row in range(rowOffset, rowLen):
+                for col in range(colOffset, colLen):
+                    expMat[row][col] = mat[r][c]
 
-                for row in range(rowOffset, rowLen):
-                    for col in range(colOffset, colLen):
-                        self.expMat[row][col] = mat[r][c]
-
-    def printMatrix(self):
-        for r in range(len(self.expMat)):
-            for c in range(len(self.expMat[0])):
-                print(self.expMat[r][c], end = " ")
-            print()
+    return expMat
+    
+def printMatrix(mat):
+    for r in range(len(mat)):
+        for c in range(len(mat[0])):
+            print(mat[r][c], end = " ")
+        print()
 
 mat = [
     [1, 2, 3],
     [4, 5, 6]
 ]
 
-expMat = MatrixExpander(mat, 2, 2)
+expMat = expandMatrix(mat, 2, 2)
 
-expMat.printMatrix()
+printMatrix(expMat)
 
