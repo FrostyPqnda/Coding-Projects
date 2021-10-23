e5a58975648bdc4aeffe100be85285506c1caf2d
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-10-23 14:32:07 -0500

    Created WordBlock project

---
+++ Java/WordBlock/WordBlock.java
@@ -0,0 +1,81 @@
+/**
+ * @author Brian Pham | FrostyPqnda
+ * @version 1.0
+ * 
+ * WordBlock class.
+ * 
+ * Given a String, row value, and column value, 
+ * create a character block (2D Array) of size
+ * row value * column value [row value][column value]
+ * and fill it up with each character in the String.
+ * 
+ * Each '-' at the end of the block denotes existence
+ * of a whitespace within the original String, or to
+ * compensate the block if its size is greater than
+ * the length of the String.
+ */
+public class WordBlock
+{
+    // Private instance variable for the WordBlock class
+    private char[][] charBlock;
+
+    /**
+     * Constructor for the WordBlock class.
+     * 
+     * Creates a character block with each element
+     * filled by a character in the given String.
+     * 
+     * @param word -> String that will be used to fill up the character block
+     * @param row -> Row size of the character block
+     * @param col -> Column size of the character block
+     */
+    public WordBlock(String word, int row, int col) 
+    {
+        this.charBlock = new char[row][col];
+        int index = 0;
+        
+        for(int r = 0; r < row; r++) 
+        {
+            for(int c = 0; c < col; c++)
+            {
+                String newText = word.replace(" ", "");
+                boolean inBound = index >= 0 && index < newText.length();
+                
+                if(inBound) {
+                    charBlock[r][c] = newText.charAt(index);
+                } else {
+                    charBlock[r][c] = '-';
+                }
+
+                index++;
+            }
+        }
+    }
+
+    /**
+     * @return the character block (2D array)
+     */
+    public char[][] getWordBlock() 
+    {
+        return charBlock;
+    }
+
+    /**
+     * @return the character block as an object String
+     */
+    public String toString() 
+    {
+        String blockStr = "";
+
+        for(int r = 0; r < charBlock.length; r++)
+        {
+            for(int c = 0; c < charBlock[r].length; c++)
+            {
+                blockStr += charBlock[r][c] + " ";
+            }
+            blockStr += System.lineSeparator();
+        }
+
+        return blockStr;
+    }
+}
---
+++ Java/WordBlock/WordBlockTester.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class WordBlockTester 
+{
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.print("Enter a word: ");
+        String word = scan.nextLine();
+        int row, col;
+        WordBlock wb;
+
+        do {
+            row = readRow();
+            col = readColumn();
+            
+            wb = new WordBlock(word, row, col);
+            System.out.println("\n" + wb);
+        } while(row < 0 || col < 0);
+        
+    }
+
+    static int readRow() 
+    {
+        int row;
+
+        do {
+            System.out.print("Enter a row value: ");
+            row = scan.nextInt();
+
+            if(row < 0) {
+                System.out.println("Row value must be 0 or greater!");
+            }
+        } while(row < 0);
+
+        return row;
+    }
+
+    static int readColumn()
+    {
+        int column;
+
+        do {
+            System.out.print("Enter a column value: ");
+            column = scan.nextInt();
+
+            if(column < 0) {
+                System.out.println("Column value must be 0 or greater!");
+            }
+        } while(column < 0);
+
+        return column;
+    }
+}
