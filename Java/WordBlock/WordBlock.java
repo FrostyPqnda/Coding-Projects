637f7684ad369513ddb13f23decca2fbc7b51126
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2021-10-24 19:01:02 -0500

    Update WordBlock.java

--- Java/WordBlock/WordBlock.java
+++ Java/WordBlock/WordBlock.java
@@ -73,7 +73,7 @@ public class WordBlock
             {
                 blockStr += charBlock[r][c] + " ";
             }
-            blockStr += System.lineSeparator();
+            blockStr += "\n";
         }
 
         return blockStr;
