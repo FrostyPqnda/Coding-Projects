6a4b50fb7fca5b70464385feb7b6df5436a65d5e
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2022-02-01 01:44:56 -0600

    Update KeyLogger.py

--- Python/KeyLogger/KeyLogger.py
+++ Python/KeyLogger/KeyLogger.py
@@ -17,40 +17,54 @@ keyArr = []
 # Checks if the key logger is still running
 isRunning = True
 
-# Creates a GUI window
-root = Tk()
-
+# Creates a GUI app
+app = Tk()
+title = Label(app, font = ('Oswald', 40, 'bold'), background = '#283140', foreground = '#cccccc', text='Key Logger')
+canvas = Canvas(app, width = 600, height = 600, bg = '#1d4d80')
+headerText = Label(canvas, font = ('Oswald', 20, 'bold'), text = 'Press any key to start the recording.\nClose the application to end the recording', bg = '#1d4d80', fg = '#fff')
+keyPressText = Text(canvas, bg = '#222', fg = "#fff", font = ('Oswald', 20, 'bold'), height = 10, width = 50)
 
 # Destroys the GUI app on exit
-def exit():
+def closeApp():
     global isRunning
     msgBox = messagebox.askquestion('Exit Application','Do you wish to end the recording?')
 
+    # End the recording if the user decides to close the app.
     if msgBox == 'yes':
-        root.destroy()
+        app.destroy()
         isRunning = False
 
 # Creates a pop-up GUI app
-def createPopUpWindow():
-    root.title('KeyLogger')
-    root.resizable(False, False)
-    canvas = Canvas(root, width = 300, height = 300, bg = '#1d4d80')
-    canvas.pack()
-    button = Button(root, text = 'End Recording', font = ('Oswald', 12), command = exit, bg = '#222222', fg = '#ffffff', width = 25, height = 5)
-    canvas.create_window(150, 150, window = button)
-    root.protocol("WM_DELETE_WINDOW", exit)
-    root.mainloop()
+def drawApp():
+    app.title('KeyLogger')
+    app.iconbitmap('Icon.ico')
+    app.minsize(520, 710)
+    app.geometry("%dx%d" % (app.winfo_screenwidth(), app.winfo_screenheight()))
+    app.unbind_all('<<PrevWindow>>')
+    app.unbind_all('<<NextWindow>>')
+    app.protocol("WM_DELETE_WINDOW", closeApp)
+    app.state('zoomed')
+
+    title.pack(fill = X)
+    canvas.pack(expand=True, fill='both')
+    headerText.pack(side = TOP, pady = 20)
+    keyPressText.pack(side = TOP, padx = 10)
+
+    app.mainloop()
 
 # Record key presses and append them to a list
 def keyPress(key):
     try:
-        print('Key {0} pressed'.format(key.char))
+        keyPressText.insert(END, f' Key pressed: {key.char}\n')
 
         if (key.char).isalpha():
             keyArr.append((key.char).upper())
 
     except AttributeError:
-        print('Key {0} pressed'.format(key))
+        keyPressText.insert(END, f' Key pressed: {key}\n')
+    
+    # Scrolls the text box to tahe end
+    keyPressText.see("end")
 
 # Count the frequency of each key input and store
 # it into a bucket.
@@ -65,9 +79,8 @@ def bucketize(data, bucket):
 
 # Records the key logging session
 def recordKey():
-    print('KeyLogger recording in session [Close the pop-up window to end the recording]:')
     with kb.Listener(on_press = keyPress) as listener:
-        createPopUpWindow()
+        drawApp()
         
         if not isRunning:
             return False
