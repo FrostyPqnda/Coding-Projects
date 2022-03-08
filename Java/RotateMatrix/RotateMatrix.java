a54743fcdbf994097d27290d93cc351262aa0df6
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2022-03-07 18:19:24 -0600

    Create RotateMatrix.java

---
+++ Java/RotateMatrix/RotateMatrix.java
@@ -0,0 +1,12 @@
+public class RotateMatrix 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello world");
+    }
+
+    static void rotate(int[][] matrix, int degree) 
+    {
+        
+    }
+}
