fbed194af7aca637492b3c0645306024a6748563
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2022-05-18 23:49:26 -0500

    Update RotateMatrix.java

--- Java/RotateMatrix/RotateMatrix.java
+++ Java/RotateMatrix/RotateMatrix.java
@@ -2,11 +2,51 @@ public class RotateMatrix
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello world");
+
+        int[][] mat = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        System.out.println("Original");
+        print(mat);
+
+        rotate(mat);
+        System.out.println("\nNew");
+        print(mat);
     }
 
-    static void rotate(int[][] matrix, int degree) 
+    /**
+     * @param mat
+     * 
+     * Rotate a matrix by 90 degrees in a 
+     * counterclockwise rotation.
+     */
+    static void rotate(int[][] mat) 
     {
-        
+        for(int i = 0; i < mat.length / 2; i++) {
+            for(int j = i; j < mat.length - i - 1; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][mat.length - 1 - i];
+                mat[j][mat.length - 1 - i] = mat[mat.length - 1 - i][mat.length - 1 - j];
+                mat[mat.length - 1 - i][mat.length - 1 - j] = mat[mat.length - 1 - j][i];
+                mat[mat.length - 1 - j][i] = temp;
+            }
+        }
+    }
+
+    /**
+     * @param mat
+     * 
+     * Print the matrix
+     */
+    static void print(int[][] mat) {
+        for(int[] arr : mat) {
+            for(int item : arr) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
     }
 }
