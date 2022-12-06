7192197f427907a284728b67d5077017937851ff
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2022-12-05 22:03:01 -0600

    Create PrimeSieve.py

---
+++ Python/PrimeSieve.py
@@ -0,0 +1,27 @@
+"""
+Returns a list of prime number up to n using
+the Sieve of Eratosthenes algorithm.
+
+Time Complexity: O(nlog(log(n)))
+Learn more: [https://www.geeksforgeeks.org/sieve-of-eratosthenes/]
+"""
+def getPrime(n: int):
+    if not isinstance(n, int):
+        return 'Invalid data type'
+
+    if n < 2:
+        return []
+
+    marked = [True for _ in range(n + 1)]
+    len = (int)(n ** 0.5)
+
+    for i in range(2, len + 1):
+        if marked[i]:
+            for j in range(i * i, n + 1, i):
+                marked[j] = False
+    
+    # Return a list using list comprehension
+    return [k for k in range(2, n + 1) if marked[k]]
+    
+n = int(input('Enter a number: '))
+print(getPrime(n))
