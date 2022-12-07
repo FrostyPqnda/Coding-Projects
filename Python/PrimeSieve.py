9d08558c8a79bdb2b425d2cfbc9b961fb67aed17
FrostyPqnda <58892538+FrostyPqnda@users.noreply.github.com>
2022-12-06 20:45:02 -0600

    Update PrimeSieve.py

--- Python/PrimeSieve.py
+++ Python/PrimeSieve.py
@@ -6,9 +6,6 @@ Time Complexity: O(nlog(log(n)))
 Learn more: [https://www.geeksforgeeks.org/sieve-of-eratosthenes/]
 """
 def getPrime(n: int):
-    if not isinstance(n, int):
-        return 'Invalid data type'
-
     if n < 2:
         return []
 
@@ -20,8 +17,9 @@ def getPrime(n: int):
             for j in range(i * i, n + 1, i):
                 marked[j] = False
     
-    # Return a list using list comprehension
+    # Return a list of prime using list comprehension
     return [k for k in range(2, n + 1) if marked[k]]
     
-n = int(input('Enter a number: '))
-print(getPrime(n))
+if __name__ == '__main__':
+    n = int(input('Enter a number: '))
+    print(getPrime(n))
