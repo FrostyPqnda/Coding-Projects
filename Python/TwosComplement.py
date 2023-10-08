80fa007ab10290aa050170df0c4fc75ba3eefbb4
Brian Pham <58892538+FrostyPqnda@users.noreply.github.com>
2023-10-07 22:37:19 -0500

    Create TwosComplement.py

---
+++ Python/TwosComplement.py
@@ -0,0 +1,80 @@
+import sys
+
+# Convert decimal to binary
+def bin(value: int):
+    if value < 0:
+        print('Value must be at least 0')
+        sys.exit()
+    
+    s = ''
+    while value > 0:
+        s += str(value % 2)
+        value = int(value / 2)
+
+    return format(('0' * 4) + s[::-1])
+
+# Return the sign bit of binary number [Leftmost value]
+# 0 -> Positive
+# 1 -> Negative
+def sign(b: str) -> str:
+    return b[0]
+
+# Format to make length of binary string divisible by 4 
+# and add spacing every 4 digits
+def format(a: str) -> str:
+    flag = len(a) % 4 != 0
+    if flag:
+        i = 1
+        while flag:
+            a = a.rjust(i, '0')
+            i += 1
+            flag = len(a) % 4 != 0 
+    
+    a = ' '.join(a[i: i + 4] for i in range(0, len(a), 4))
+    return a
+
+# Inverts the binary number
+# 0 -> 1
+# 1 -> 0
+def invert(bin: str):
+    inv = ''
+    for i in range(len(bin)):
+        if bin[i] == '1':
+            inv += '0'
+        elif bin[i] == '0':
+            inv += '1'
+    return inv
+
+# Returns two's complement of a binary number
+def complement(bin: str):
+    inv = invert(bin)
+    a = '1'.zfill(len(inv))
+
+    res = ''
+    carry = 0
+
+    for i in range(len(inv) - 1, -1, -1):
+        r = carry
+        r += 1 if a[i] == '1' else 0
+        r += 1 if inv[i] == '1' else 0
+        res = ('1' if r % 2 == 1 else '0') + res
+        carry = 0 if r < 2 else 1
+    
+    if carry != 0:
+        res = '1' + res
+    
+    res = format(res)
+    
+
+    return res
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        file = __file__
+        file = file[file.rfind('\\') + 1:]
+        print(f'Usage {file} <decimal value>')
+        sys.exit()
+    
+    value = bin(int(sys.argv[1]))
+    print(value)
+    print(complement(value))
