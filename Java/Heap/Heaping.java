68af2a343383e82169f7b5a18a5037ca4c31a53a
Brian Pham <58892538+FrostyPqnda@users.noreply.github.com>
2023-10-28 12:01:20 -0500

    Update Heaping.java

--- Java/Heap/Heaping.java
+++ Java/Heap/Heaping.java
@@ -1,13 +1,14 @@
 public class Heaping {
     public static void main(String[] args) {
         int[] data = {12, 8, 5, 15, 7, 2, 9};
-        Heap<Integer> hp = new BinomialQueue<>();
+        Heap<Integer> hp = new BinaryHeap<>();
         
         for(int item : data)
             hp.insert(item);
 
-        hp.print();     
-        System.out.println(hp.deleteMin());
-        hp.print();       
+
+        System.out.println(hp); 
+        hp.deleteMin();
+        System.out.println(hp);
     }
 }
