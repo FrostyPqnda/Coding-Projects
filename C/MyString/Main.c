7062325a16b0254cb91e89b89246711853a80b3e
Brian Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-01-15 20:52:42 -0600

    Update Main.c

--- C/MyString/Main.c
+++ C/MyString/Main.c
@@ -5,10 +5,10 @@
 #include "MyString.h"
 
 int main() {
-    MyString myStr = init("Applesauce Glider");
+    MyString myStr = init("the quick brown fox jumps over the lazy dog");
     printf("%s\n", toString(myStr));
 
-    MyString* split = split_MyString(myStr, init(" "));
+    MyString* split = split_MyString(myStr, init("brown"));
     int len = split_length(split);
     for(int i = 0; i < len; i++)
         printf("%s,", split[i]);
