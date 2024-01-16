b83c36cfd15e80c86c24d4d9abc4e5bc19a921cb
Brian Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-01-16 16:47:01 -0600

    Update Main.c

--- C/MyString/Main.c
+++ C/MyString/Main.c
@@ -8,8 +8,10 @@ int main() {
     MyString myStr = init("the quick brown fox jumps over the lazy dog");
     printf("%s\n", toString(myStr));
 
-    MyString* split = split_MyString(myStr, init(" "));
+    MyString* split = split_char(myStr, ' ');
     int len = split_length(split);
     for(int i = 0; i < len; i++)
         printf("%s\n", split[i]);
+
+    printf("%d\n", len);
 }
