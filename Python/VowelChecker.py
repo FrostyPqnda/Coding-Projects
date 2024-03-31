86a71c06dbfb4b783c02ef6db02dceb9a1506c4e
Brian Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-03-30 22:22:12 -0500

    Update VowelChecker.py

--- Python/VowelChecker.py
+++ Python/VowelChecker.py
@@ -7,14 +7,12 @@ Created on May 31, 2020
 Python program that asks an input from the user
 to check if there is a vowel.
 """
+
+vowels = ['A', 'E', 'I', 'O', 'U']
+
 vowel = input("Enter a string of letters: ")
 def check_for_vowel(vowel):
-    if "a" in vowel or "e" in vowel or "i" in vowel or "o" in vowel or "u" in vowel: 
-        return "Contains a vowel!"
-    if "A" in vowel or "E" in vowel or "I" in vowel or "I" in vowel or "U" in vowel:
-        return "Contains a vowel!"
-    else:
-        return "Does not contain a vowel."
+    return any(letter.upper() in vowels for letter in vowel)
 
-print (check_for_vowel(vowel))
+print(check_for_vowel(vowel))
 
