50686743bf8b4352a6381c4333703c5dda33cb63
Brian-Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-06-09 16:57:52 -0500

    Update Stacking.c

--- C/Stack/Stacking.c
+++ C/Stack/Stacking.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "Stack.h"
+#include "Stack.c"
 
+/**
+ * Compile: gcc Stacking.c -o <Output File>
+ * Execute: ./<Output File>
+ */
 int main() {
     Stack* stack = NULL;
     push(&stack, 10);
