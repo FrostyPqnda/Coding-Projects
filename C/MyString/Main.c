a9aad47632e49592ebfb7e0894ad0da6712f3039
Brian-Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-06-09 17:07:06 -0500

    Update Main.c

--- C/MyString/Main.c
+++ C/MyString/Main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "MyString.h"
+#include "MyString.c"
 
+/**
+ * Compile: gcc Main.c -o <Output File>
+ * Execute: ./<Output File>
+ */
 int main() {
     MyString myStr = init("the quick brown fox jumps over the lazy dog");
     printf("%s\n", toString(myStr));
