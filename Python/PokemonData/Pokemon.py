818b8b6feea2387614ef26eaf46eb74e3cf899e8
Brian-Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-06-09 16:10:56 -0500

    Cleaned up Pokemon.py and PokemonData.py

--- Python/PokemonData/Pokemon.py
+++ Python/PokemonData/Pokemon.py
@@ -14,20 +14,13 @@ class Pokemon:
     # Constructor for the Pokemon class
     # Order: Name, Type, Ability, Shiny, HP, Level
     # Type: String, String, String, Boolean, Int, Int
-    def __init__(self, pkmnName = None, pkmnType = None, pkmnAbility = None, pkmnShiny = None, pkmnHP = 0, pkmnLevel = 0):
+    def __init__(self, pkmnName: str = 'MissingNo.', pkmnType: str = None, pkmnAbility: str = None, pkmnHP: int = 0, pkmnLevel: int= 0):
         self.pkmnName = pkmnName
         self.pkmnType = pkmnType
         self.pkmnAbility = pkmnAbility
-        self.pkmnShiny = pkmnShiny
         self.pkmnHP = pkmnHP
         self.pkmnLevel = pkmnLevel
     
     # Returns the object as a string
     def __str__(self):
-        pkmnName = "Pokemon: " + str(self.pkmnName)
-        pkmnType = "\nType: " + str(self.pkmnType)
-        pkmnAbility = "\nAbility: " + str(self.pkmnAbility)
-        pkmnShiny = "\nShiny: " + str(self.pkmnShiny)
-        pkmnHP = "\nHP: " + str(self.pkmnHP)
-        pkmnLevel = "\nLevel: " + str(self.pkmnLevel)
-        return pkmnName + pkmnType + pkmnAbility + pkmnShiny + pkmnHP + pkmnLevel
+        return f'Pokemon: {self.pkmnName}\nType: {self.pkmnType}\nAbility: {self.pkmnAbility}\nHP: {self.pkmnHP}\nLevel: {self.pkmnLevel}'
--- Python/PokemonData/PokemonData.py
+++ Python/PokemonData/PokemonData.py
@@ -10,13 +10,13 @@ to a .txt file
 from Pokemon import Pokemon # Imports the Pokemon.py file to use the Pokemon class
 from random import randint # Imports the randint function from the random module
 
-salamence = Pokemon("Salamence", "Dragon, Flying", "Intimidate, Moxie", True, randint(202, 394), randint(50, 100))
-charizard = Pokemon("Charizard", "Fire, Flying", "Blaze, Solar Power", True, randint(185, 360), randint(50, 100))
-lucario = Pokemon("Lucario", "Fighting, Steel", "Inner Focus, Justified", True, randint(177, 344), randint(50, 100))
-blastoise = Pokemon("Blastoise", "Water", "Torrent, Rain Dish", True, randint(186, 362), randint(50, 100))
-gengar = Pokemon("Gengar", "Ghost, Poison", "Cursed Body", True, randint(167, 324), randint(50, 100))
-infernape = Pokemon("Infernape", "Fire, Fighting", "Blaze, Iron Fist", True, randint(183, 356), randint(50, 100))
-gyarados = Pokemon("Gyarados", "Water, Flying", "Intimidate, Moxie", True, randint(202, 394), randint(50, 100))
+salamence = Pokemon("Salamence", "Dragon, Flying", "Intimidate, Moxie", randint(202, 394), randint(50, 100))
+charizard = Pokemon("Charizard", "Fire, Flying", "Blaze, Solar Power", randint(185, 360), randint(50, 100))
+lucario = Pokemon("Lucario", "Fighting, Steel", "Inner Focus, Justified", randint(177, 344), randint(50, 100))
+blastoise = Pokemon("Blastoise", "Water", "Torrent, Rain Dish", randint(186, 362), randint(50, 100))
+gengar = Pokemon("Gengar", "Ghost, Poison", "Cursed Body", randint(167, 324), randint(50, 100))
+infernape = Pokemon("Infernape", "Fire, Fighting", "Blaze, Iron Fist", randint(183, 356), randint(50, 100))
+gyarados = Pokemon("Gyarados", "Water, Flying", "Intimidate, Moxie", randint(202, 394), randint(50, 100))
 pk = Pokemon()
 
 # Creates a list of Pokemon objects to store
@@ -32,19 +32,12 @@ pkmnList.append(infernape)
 pkmnList.append(gyarados)
 pkmnList.append(pk)
 
-# Creates the 'PokemonData.txt' file and
-# will overwrite any pre-existing data
+# Creates the 'PokemonData.txt' file
+# Will overwrite any pre-existing data
 # Note: any pre-existing data about a Pokemon will be overwrittten
-pkmn_data_file = 'PokemonData.txt'
-data = open(pkmn_data_file, 'w')
+with open('PokemonData.txt', 'w') as pk:
+    for pkmn in pkmnList:
+        pk.write(str(pkmn))
+        pk.write('\n=====================================\n')
 
-# Writes all the Pokemon objects in the pkmnList to
-# the 'PokemonData.txt' file
-for pkmn in pkmnList:
-    data.write((str)(pkmn) + "\n")
-    data.write("=====================================")
-    data.write("\n")
-
-# Closes the file after all objects have been added
-data.close()
-print("Pokemon data successfully written to " + pkmn_data_file)
+print("Pokemon data successfully written to PokemonData.txt")
