7ceb366dc4890f6d94db3229e59e5242a1090aba
Brian-Pham <58892538+FrostyPqnda@users.noreply.github.com>
2024-07-04 17:32:51 -0500

    Deleted old files

--- C#/ShuntingYard/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program {
-    public static void Main(string[] args) {
-        ShuntingYard s = new ShuntingYard("12 + 3 * (3 ^ 2 - 3) ^ (4 + 10 * 5) - 5");
-        string k = s.ToPostfix();
-        Console.WriteLine(k);
-        Console.ReadKey();
-    }
-}
