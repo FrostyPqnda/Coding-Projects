67ea0f911d51abdc438ab15a29842aa0ee4ccf1d
Brian Pham <frostypqnda@gmail.com>
2025-08-09 00:51:12 -0500

    Created Deque project

---
+++ Go/Deque/Deque.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	ll "deque/myimplementations"
+)
+
+/*
+Implementation of the Deque data structure
+utilizing doubly linked list
+*/
+type Deque[T comparable] struct {
+	list *ll.LinkedList[T] // pointer
+}
+
+// Instantiates a deque using a doubly linked list
+func NewDeque[T comparable]() *Deque[T] {
+	return &Deque[T]{
+		list: ll.NewList[T](),
+	}
+}
+
+// Insert at the front of the deque
+func (d *Deque[T]) InsertFront(data T) {
+	d.list.AddHead(data)
+}
+
+// Insert at the rear of the deque
+func (d *Deque[T]) InsertRear(data T) {
+	d.list.AddTail(data)
+}
+
+// Delete the front of the deque
+func (d *Deque[T]) DeleteFront() {
+	d.list.RemoveHead()
+}
+
+// Delete the front of the deque
+func (d *Deque[T]) DeleteRear() {
+	d.list.RemoveTail()
+}
+
+// Get the front of the deque
+func (d *Deque[T]) GetFront() T {
+	return d.list.GetHead()
+}
+
+// Get the tail of the deque
+func (d *Deque[T]) GetTail() T {
+	return d.list.GetTail()
+}
+
+// Check if the deque is empty
+func (d *Deque[T]) IsEmpty() bool {
+	return d.list.IsEmpty()
+}
+
+// Get the size of the deque
+func (d *Deque[T]) Size() int {
+	return d.list.Size()
+}
+
+// Get the string value of the deque
+func (d *Deque[T]) String() string {
+	return d.list.String()
+}
---
+++ Go/Deque/go.mod
@@ -0,0 +1,3 @@
+module deque
+
+go 1.24.5
